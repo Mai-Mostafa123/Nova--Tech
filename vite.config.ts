118ca9381f306e237d1eca9c5800ade00d2c986a
Mai-Mostafa123 <maimm5450@gmail.com>
2026-04-03 00:53:01 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: false,
+
+      // خليه true دايمًا عشان ميعملش مشاكل
+      hmr: true,
+
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3100',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+
+      // 👇 أهم تعديل (حل المشكلة)
+      watch: {
+        ignored: ['**/data/store.json'],
+      },
+    },
+  };
+});
